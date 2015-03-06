3bf7338cf1f2c14b20b8f21479d07b013cd87e7b
unknown <khamgaonkarg@DPCT210387.premiers.qld.gov.au>
2015-03-06 15:57:06 +1000

    inserted data

---
+++ 00helloworld/src/main/java/com/sanchii/java/spring/hellowworld_305/bean/HelloWorld.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package com.sanchii.java.spring.hellowworld_305.bean;
+
+/**
+ * 
+ * 
+ * @author khamgaonkarg
+ *
+ */
+public class HelloWorld {
+	
+	private String name;
+	 
+	public void setName(String name) {
+		this.name = name;
+	}
+ 
+	public void printHello() {
+		System.out.println("Hello ! " + name);
+	}
+
+}
